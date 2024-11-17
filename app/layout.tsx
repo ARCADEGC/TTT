e855b3cc48e8e3c5772dd0a82340c85c5f76814e
Jaromír Trnka <jaromir.trnka@protonmail.com>
2024-11-17 21:52:46 +0100

    tailwind plugins & clear main

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,7 @@ import "./globals.css";
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
     variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+    weight: "400 600 950",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body className={`${geistSans.variable} bg-background text-foreground antialiased`}>{children}</body>
         </html>
     );
 }
