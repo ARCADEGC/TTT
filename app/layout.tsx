a4ecbf8b75288322c31b66ca84847efa409d8cb4
Jaromír Trnka <jaromir.trnka@protonmail.com>
2024-11-22 17:25:15 +0100

    Million

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { CSPostHogProvider } from "@/app/_analytics/posthogProvider";
+import { CSPostHogProvider } from "@/app/analytics/posthogProvider";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
