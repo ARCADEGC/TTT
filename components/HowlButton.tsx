cacc46350b9b0326bde1fc94069febdb48138cb0
Jaromír Trnka <jaromir.trnka@protonmail.com>
2024-12-22 15:58:02 +0100

    Toaster & Input, Howl Button Component, Prettier & Deps,

---
+++ components/HowlButton.tsx
@@ -0,0 +1,31 @@
+import { Howl } from "howler";
+
+import { Button } from "@/components/ui/button";
+
+export function HowlButton() {
+    const buttonOnClickAudio = new Howl({
+        src: ["/audio/audioOnClickTest.wav"],
+    });
+
+    const buttonOnHoverAudio = new Howl({
+        src: ["/audio/audioOnHoverTest.wav"],
+    });
+
+    function handleOnClick() {
+        buttonOnClickAudio.play();
+    }
+
+    function handleOnHover() {
+        buttonOnHoverAudio.play();
+    }
+
+    return (
+        <Button
+            onClick={handleOnClick}
+            onMouseEnter={handleOnHover}
+            variant={"outline"}
+        >
+            Play
+        </Button>
+    );
+}
